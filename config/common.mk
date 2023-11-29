59c3cf4ca319ce513b8eb579313179b39d50e64b
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2023-11-29 23:14:01 +0000

    Config: add flags for GAPPS
    Co-Authored-By: Priyanshu <shen.priyanshu@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -5,6 +5,14 @@ PRODUCT_BRAND ?= Ocean-UI
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+OCEAN_ZIP_TYPE := Vanilla
+
+# Gapps
+ifeq ($(OCEAN_GAPPS), true)
+    $(call inherit-product, vendor/gms/gms_full.mk)
+    OCEAN_ZIP_TYPE := Gapps
+endif
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
